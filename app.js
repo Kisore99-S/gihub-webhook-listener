b814f18254349f59c1c5dcd517577e0973bdc537
Kisore <kisore.s70@gmail.com>
2025-06-28 23:58:06 +0530

    update app

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ import webhookRoutes from "./routes/webhook.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf;
+    },
+  })
+);
 
 app.use("/webhook", webhookRoutes);
 
